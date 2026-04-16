5423dadd520be84f3c4c69ba516c252b81e5d965
dctn <dctngamers@gmail.com>
2026-04-16 19:13:34 +0530

    add ear and head_pos with fusion score

--- media.py
+++ media.py
@@ -100,17 +100,23 @@ def logger(type,value):
 inner = 133 #right
 outer = 33 #left
 left_iris = 468
+
 top = 10
 bottom = 152
+
+LEFT_EYE_TOP    = 159
+LEFT_EYE_BOTTOM = 145
+
 ratios = []
 
 generate_random_color()
 
 
-smooth_ratio   = 0.5
-smooth_v_ratio = 0.5
+smooth_ratio   = None
+smooth_v_ratio = None
+smooth_ear = None
+smooth_head_pos = None
 ALPHA = 0.25
-DEAD = 0.03
 
 # %%
 """
@@ -169,7 +175,7 @@ class IrisCalibration:
         self.samples = {}
         self.thresholds = {}
         self.is_calibrated = False
-        self.n_sample = 50*6
+        self.n_sample = 50*3
         self.current_idx = 0
 
     def get_points(self,input_frame):
@@ -179,29 +185,34 @@ class IrisCalibration:
         points = {
             'center':{
                 'point':(screen_CX, screen_CY),
-                'samples':[]
+                'samples':[],
+                'other_samples':[]
             },
             'left':{
                 'point':(50, screen_CY),
-                'samples':[]
+                'samples':[],
+                'other_samples':[]
             },
             'right':{
                 'point':(screen_w - 150, screen_CY),
-                'samples':[]
+                'samples':[],
+                'other_samples':[]
             },
             'top':{
                 'point':(screen_CX, 50),
-                'samples':[]
+                'samples':[],
+                'other_samples':[]
             },
             'bottom':{
                 'point':(screen_CX, screen_h - 50),
-                'samples':[]
+                'samples':[],
+                'other_samples':[]
             }
         }
 
         return points
 
-    def collect(self,input_frame,ratio,v_ratio):
+    def collect(self,input_frame,ratio,v_ratio,ear,head_pos):
         if self.points is None:
             self.points = self.get_points(input_frame)
 
@@ -222,47 +233,48 @@ class IrisCalibration:
         cv2.circle(input_frame,current_point['point'],5,(0,0,255),-1)
 
         if len(current_point['samples']) < self.n_sample:
-            current_point['samples'].append((ratio,v_ratio))
+            current_point['samples'].append((ratio,v_ratio,ear,head_pos))
             cv2.putText(input_frame, f"{len(current_point['samples'])}: ", (30, 40), cv2.FONT_ITALIC,1, (0,0,255), 2)
         elif len(current_point['samples']) >= self.n_sample:
             self.current_idx += 1
 
 
 
+    def ratio_mean(self,label,sample='samples'):
+        data = self.points[label][sample]
+        ratio_avg = sum(label_ratio for label_ratio,_,_,_ in data) / len(data)
+        v_ratio_avg = sum(i for _,i,_,_ in data) / len(data)
 
-
-    def ratio_mean(self,label):
-        data = self.points[label]['samples']
-        ratio_avg = sum(label_ratio for label_ratio,_ in data) / len(data)
-        v_ratio_avg = sum(i for _,i in data) / len(data)
-        return ratio_avg, v_ratio_avg
-
-
-    def percentile(self, data, p):
-        sorted_data = sorted(data)
-        idx = (p / 100) * (len(sorted_data) - 1)
-        lo  = int(idx)
-        hi  = min(lo + 1, len(sorted_data) - 1)
-        return sorted_data[lo] + (sorted_data[hi] - sorted_data[lo]) * (idx - lo)
-
+        ear_avg = sum(ear for _,_,ear,_ in data) / len(data)
+        head_pos_avg = sum(head for _,_,_,head in data) / len(data)
+        return ratio_avg, v_ratio_avg, ear_avg,head_pos_avg
 
     def calculate_threshold(self):
-        left_h   = [r for r, _ in self.points['left']['samples']]
-        right_h  = [r for r, _ in self.points['right']['samples']]
-        center_h = [r for r, _ in self.points['center']['samples']]
-        top_v    = [v for _, v in self.points['top']['samples']]
-        bottom_v = [v for _, v in self.points['bottom']['samples']]
-        center_v = [v for _, v in self.points['center']['samples']]
+        center_h,center_v,center_ear_avg,center_head_pos_avg = self.ratio_mean('center')
+        left_h,left_v,_,_ = self.ratio_mean('left')
+        right_h,right_v,_,_ = self.ratio_mean('right')
+        _,top_v,_,top_head_pos_avg = self.ratio_mean('top')
+        _,bottom_v,_,bottom_head_pos_avg = self.ratio_mean('bottom')
+
+        head_pitch_top = abs(center_head_pos_avg - top_head_pos_avg)
+        head_pitch_bottom = abs(center_head_pos_avg - bottom_head_pos_avg)
 
 
-
-
+        buf = 0.05
+        v_buf = 0.30
         self.thresholds = {
-            'left_thresh':  (self.percentile(left_h,   85) + self.percentile(center_h, 15)) / 2,
-            'right_thresh': (self.percentile(center_h, 85) + self.percentile(right_h,  15)) / 2,
-            'top_thresh':   (self.percentile(top_v,    85) + self.percentile(center_v, 15)) / 2,
-            'down_thresh':  (self.percentile(center_v, 85) + self.percentile(bottom_v, 15)) / 2,
+            'left_thresh':  center_h - (center_h - left_h)  * (1 - buf),
+            'right_thresh': center_h + (right_h - center_h) * (1 - buf),
+            'top_thresh':    center_v - (center_v - top_v)   * (1 - v_buf),
+            'down_thresh':  center_v + (bottom_v - center_v)   * (1 - v_buf),
+            'ear_thresh':  center_ear_avg * 0.75,
+            'head_pos_thresh': max(head_pitch_top,head_pitch_bottom) * 0.7,
+            'center_ratio': center_h,
+            'center_v_ratio': center_v,
+            'center_head_pos': center_head_pos_avg,
+            'center_ear':center_ear_avg,
         }
+        # self.is_calibrated = True
         return self.thresholds
 
     def calibration_accuracy(self):
@@ -281,25 +293,14 @@ class IrisCalibration:
             points_samples = self.points[point]['samples']
             label_correct = 0
             true_h,true_v = true_points[point]
-            threshold_values = self.thresholds
-            for ratio,v_ratio in points_samples:
+            for ratio,v_ratio,ear,head_pos in points_samples:
                 total += 1
 
-                if ratio < threshold_values['left_thresh']:
-                    pred_h = 'left'
-                elif ratio > threshold_values['right_thresh']:
-                    pred_h = 'right'
-                else:
-                    pred_h = 'center'
+                score = fusion_score(ratio,v_ratio,ear,head_pos,self.thresholds)
 
-                if v_ratio < threshold_values['top_thresh']:
-                    prev_v = 'top'
-                elif v_ratio > threshold_values['down_thresh']:
-                    prev_v = 'bottom'
-                else:
-                    prev_v = 'center'
-
-                if true_h == pred_h and true_v == prev_v:
+                if point == 'center' and score < 0.4:
+                    label_correct += 1
+                elif point != 'center' and score < 0.6:
                     label_correct += 1
 
             label_report[point] = label_correct
@@ -311,17 +312,76 @@ class IrisCalibration:
         return accuracy, label_report
 
 
-
 # %%
-# # for i in calibration.points:
-# print(sum([i for i,_ in calibration.points['center']['samples']]) / len([i for i,_ in calibration.points['center']['samples']]))
+import math
+def compute_ear(ff):
+    vertical   = abs(ff[LEFT_EYE_TOP].y - ff[LEFT_EYE_BOTTOM].y)
+    horizontal = abs(ff[outer].x - ff[inner].x)
+    return vertical / (horizontal + 1e-6)
 
+def compute_head_pitch(ff):
+    # Simple: how much is the nose displaced from forehead-chin midline
+    forehead_y = ff[10].y
+    chin_y     = ff[152].y
+    nose_y     = ff[1].y
+
+    midline_y = (forehead_y + chin_y) / 2
+    face_height = abs(chin_y - forehead_y)
+
+    # Positive = nose above midline = head tilted back
+    # Negative = nose below midline = head pitched forward (looking down)
+    pitch_ratio = (midline_y - nose_y) / (face_height + 1e-6)
+    return pitch_ratio  # range roughly -0.1 to +0.1
+
+# Exponential Moving Average
+def ema(current,prev=None,alpha=0.25):
+    if prev is None:
+        return current
+    return alpha * current + (1-alpha) * prev
+
+
+# Fusion score function
+def fusion_score(smooth_ratio,smooth_v_ratio,smooth_ear,smooth_head_pos,threshold_values):
+    #horizontal score
+    center_h = threshold_values['center_ratio']
+    h_range = max(abs(threshold_values['left_thresh'] - center_h),
+              abs(threshold_values['right_thresh'] - center_h))
+    h_score = abs(smooth_ratio - center_h) / h_range
+    h_score = min(1, h_score)
+
+    # vertical score
+    center_v = threshold_values['center_v_ratio']
+    v_range = max(abs(threshold_values['top_thresh'] - center_v),
+              abs(threshold_values['down_thresh'] - center_v))
+    v_score = abs(smooth_v_ratio - center_v) / v_range
+    v_score = min(1, v_score)
+
+    # ear score
+    ear_base = threshold_values['ear_thresh']
+    ear_drop = max(0,ear_base-smooth_ear)
+    ear_score = ear_drop / ear_base
+    ear_score = min(1,ear_score)
+
+    #head score
+    head_base = threshold_values['center_head_pos']
+    head_range = threshold_values['head_pos_thresh']
+    head_score = abs(smooth_head_pos - head_base) / head_range
+
+    head_score = min(1,head_score)
+
+    final_fusion_score = (
+        0.30 * h_score +
+        0.25 * v_score +
+        0.20 * ear_score +
+        0.25 * head_score
+    )
+
+    return final_fusion_score
 
 # %%
 cam = cv2.VideoCapture(0)
 
 calibration = IrisCalibration()
-threshold_values = None
 
 while cam:
     try:
@@ -340,51 +400,55 @@ while cam:
             right_conor =  ff[inner].x
             left_iris_post =  ff[left_iris].x
 
-            top_iris_post = ff[top].y
-            bottom_iris_post = ff[bottom].y
             v_left_iris_post =  ff[left_iris].y
+            left_eyelid_top = ff[LEFT_EYE_TOP].y
+            left_eyelid_bottom = ff[LEFT_EYE_BOTTOM].y
+
+            top_head_post = ff[top].y
+            bottom_head_post = ff[bottom].y
 
 
             ratio = (left_iris_post - left_conor) / ((right_conor - left_conor)  + 1e-6)
-            v_ratio = (v_left_iris_post - top_iris_post) / (bottom_iris_post - top_iris_post + 1e-6)
+            v_ratio = (v_left_iris_post - left_eyelid_top) / (left_eyelid_bottom - left_eyelid_top + 1e-6)
+            ear = compute_ear(ff)
+            head_pos = compute_head_pitch(ff)
 
-            smooth_ratio   = ALPHA * ratio   + (1 - ALPHA) * smooth_ratio
-            smooth_v_ratio = ALPHA * v_ratio + (1 - ALPHA) * smooth_v_ratio
+            smooth_ratio = ema(current=ratio,prev=smooth_ratio)
+            smooth_v_ratio = ema(current=v_ratio,prev=smooth_v_ratio)
+            smooth_ear = ema(current=ear,prev=smooth_ear)
+            smooth_head_pos = ema(current=head_pos,prev=smooth_head_pos)
 
-            calibration_values = calibration.collect(frame,smooth_ratio,smooth_v_ratio)
+            if calibration.is_calibrated == False:
+                calibration_values = calibration.collect(frame,smooth_ratio,smooth_v_ratio,smooth_ear,smooth_head_pos)
 
-            if calibration_values is not None:
-                threshold_values = calibration_values
+            if calibration.is_calibrated:
 
-            if calibration.is_calibrated and threshold_values is not None:
+                detection_score  = fusion_score(smooth_ratio,smooth_v_ratio,smooth_ear,smooth_head_pos,calibration_values)
 
-                if smooth_ratio < threshold_values['left_thresh'] - DEAD:
-                    direction = 'left'
-                elif smooth_ratio > threshold_values['right_thresh'] + DEAD:
-                    direction = 'right'
-                elif threshold_values['left_thresh'] + DEAD < smooth_ratio < threshold_values['right_thresh'] - DEAD:
-                    direction = 'center'
-                else:
-                    direction = None   # ambiguous — skip this frame
+                # if smooth_ratio < calibration_values['left_thresh']:
+                #     direction = 'left'
+                # elif smooth_ratio > calibration_values['right_thresh']:
+                #     direction = 'right'
+                # else:
+                #     direction = 'center'
+                #
+                # if smooth_v_ratio < calibration_values['top_thresh']:
+                #     v_direction = 'up'
+                # elif smooth_v_ratio > calibration_values['down_thresh']:
+                #     v_direction = 'down'
+                # else:
+                #     v_direction = 'center'
 
-                if smooth_v_ratio < threshold_values['top_thresh'] - DEAD:
-                    v_direction = 'up'
-                elif smooth_v_ratio > threshold_values['down_thresh'] + DEAD:
-                    v_direction = 'down'
-                elif threshold_values['top_thresh'] + DEAD < smooth_v_ratio < threshold_values['down_thresh'] - DEAD:
-                    v_direction = 'center'
-                else:
-                    v_direction = None
-
-                cv2.putText(frame, f"{direction}--{v_direction}: ", (30, 40), cv2.FONT_ITALIC,1, (0,0,255), 2)
-                cv2.putText(frame, f"{round(ratio,2)}--{round(v_ratio,2)}:", (30, 80), cv2.FONT_ITALIC,1, (0,0,255), 2)
+                cv2.putText(frame, f"final score--{detection_score}: ", (30, 40), cv2.FONT_ITALIC,1, (0,0,255), 2)
+                # cv2.putText(frame, f"{round(ratio,2)}--{round(v_ratio,2)}:", (30, 80), cv2.FONT_ITALIC,1, (0,0,255), 2)
 
 
-                if direction != 'center' or v_direction != 'center':
-                    # logger((direction, v_direction),(ratio,v_ratio))
+                logger(detection_score,'no cheating')
+                if detection_score > 0.6:
+                    logger(detection_score,'cheating')
                     cv2.putText(frame, 'cheating pannada loosu cutie', (w//2,h//2), cv2.FONT_ITALIC,1, (0,0,255), 2)
 
-                for face_landmarks in [left_iris, top, bottom, inner, outer]:
+                for face_landmarks in [left_iris, top, bottom, inner, outer,LEFT_EYE_BOTTOM,LEFT_EYE_TOP]:
                     cv2.circle(frame, (int(ff[face_landmarks].x*w), int(ff[face_landmarks].y*h)), 5, generate_random_color(), -1)
 
 
